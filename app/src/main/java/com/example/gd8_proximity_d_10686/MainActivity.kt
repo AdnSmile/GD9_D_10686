e6d415d1541030e0b681c6608988d640e5d8a340
AdnSmile <nessafitri16@gmail.com>
2022-10-30 23:49:30 +0700

    Start gd proximity

---
+++ app/src/main/java/com/example/gd8_proximity_d_10686/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gd8_proximity_d_10686
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
